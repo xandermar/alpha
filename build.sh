80a2ce1d0318ef65746a7e8369619d809d7cd33a
Dan Gibson <dan@dangibson.me>
2025-01-07 12:29:02 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,18 +37,4 @@ done
 
 echo "Implement all components"
 
-# echo "Implement SASS"
-# sass sass/main.css docs/main.css
 
-echo "Deploy to GitHub"
-# Check if there are any changes
-if [[ $(git status --porcelain) ]]; then
-# If there are changes, add, commit, and push
-git config --global user.email "GitHub@xandermar.com" &&
-git config --global user.name "GitHub Actions Builder" &&
-git add . &&
-git commit -m "Run BUILD" &&
-git push origin main -f
-else
-echo "No changes to commit."
-fi
