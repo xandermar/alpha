c817667a228c0c9964d4f82a375365952ee2f403
Dan Gibson <dan@dangibson.me>
2025-01-07 11:50:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ echo "Copy views to docs/"
 cp views/primary.html docs/index.html &&
 
 echo "Implement all components"
+
+echo "Deploy to GitHub"
+git add . &&
+git commit -m "Run BUILD" && 
+git push origin main -f
