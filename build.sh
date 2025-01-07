89ec814e1991ca3ff45cbfb7c24ec6094173a697
Dan Gibson <dan@dangibson.me>
2025-01-07 11:51:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ cp views/primary.html docs/index.html &&
 echo "Implement all components"
 
 echo "Deploy to GitHub"
+git config --global user.email "GitHub@xandermar.com" && 
+git config --global user.name "GitHub Actions Builder" && 
 git add . &&
 git commit -m "Run BUILD" && 
 git push origin main -f
